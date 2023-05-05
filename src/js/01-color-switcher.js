2af273a19a89c20e6d1078a1200c893896c6d2ad
landres252 <107703390+landres252@users.noreply.github.com>
2023-05-04 22:45:54 -0500

    1
    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+buttonStop.disabled = true;
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const cambioColor = () => {
+  timerId = setTimeout(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    cambioColor();
+  }, 1000)
+  buttonStart.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled')
+}
+
+const pararColor = () => {
+  clearTimeout(timerId)
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', true);
+}
+
+buttonStart.addEventListener('click', cambioColor);
+buttonStop.addEventListener('click', pararColor);
---
+++ src/js/02-timer.js
@@ -0,0 +1,65 @@
+//importar
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+let date;
+let timerId;
+
+const options = {
+  locale: 'default',
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose([selectedDates]) {
+    if (selectedDates < Date.now()) {
+      Notiflix.Report.warning(
+        'error fecha futura necesaria...ty'
+      );
+    } else {
+      startBtn.disabled = false;
+      input.disabled = true;
+      date = selectedDates;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  if (ms <= 0) {
+    input.disabled = false;
+    clearInterval(timerId);
+    return { day: '00', hour: '00', minute: '00', second: '00' };
+  }
+
+  const day = Math.floor(ms / (1000 * 60 * 60 * 24));
+  const hour = Math.floor((ms / (1000 * 60 * 60)) % 24);
+  const minute = Math.floor((ms / (1000 * 60)) % 60);
+  const second = Math.floor((ms / 1000) % 60);
+
+  const days = document.querySelector('[data-days]');
+  const hours = document.querySelector('[data-hours]');
+  const minutes = document.querySelector('[data-minutes]');
+  const seconds = document.querySelector('[data-seconds]');
+  days.textContent = `${addLeadingZero(day)}`;
+  hours.textContent = `${addLeadingZero(hour)}`;
+  minutes.textContent = `${addLeadingZero(minute)}`;
+  seconds.textContent = `${addLeadingZero(second)}`;
+
+  function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+  }
+
+  return { day, hour, minute, second };
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    convertMs(date - Date.now());
+  }, 1000);
+});
